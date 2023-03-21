98ed0b1c7e9a8e49b0401e280e110fbb9a98376e
giuseppesciacca <sciaccagi@gmail.com>
2023-03-21 16:40:27 +0100

    [add] timeout + print on html result

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,23 +5,35 @@ Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 Invece di usare prompt e allerte usate inputs ed elementi della dom per mostrare a schermo il risultato. */
 
+const botNumsEl = document.getElementById('bot_nums');
+const resultEl = document.getElementById('result');
+//il pc crea 5 numeri casuali tutti diversi e li mette in array
 const botNums = randomNum(100, 0, 5);
 console.log('Array del bot: ' + botNums);
+//dopo 30 secondi spariscono
+setTimeout(() => {
+    botNumsEl.classList.add('d_none');
+}, 30000);
+//li stampo a schermo
+printArray(botNumsEl, botNums);
 
-// prompt 5 volte e mette i numeri nell'array
-const userNums = input(5);
-console.log('Array dell\'user: ' + userNums);
+// prompt 5 volte dopo 30 secondi e mette i numeri nell'array
+setTimeout(() => {
+    // prompt 5 volte 
+    const userNums = input(5);
+    console.log('Array dell\'user: ' + userNums);
 
-// controlla se i numeri nell'userNum sono contenuti nel botNums e quante volte
-const numChecked = checkNum(userNums, botNums);
-console.log(`Hai indovinato ${numChecked} numeri`);
+    // controlla se i numeri nell'userNum sono contenuti nel botNums e quante volte
+    const numChecked = checkNum(userNums, botNums);
+    console.log(`Hai indovinato ${numChecked} numeri`);
 
+    resultEl.innerText = `Hai indovinato ${numChecked} numeri`;
+}, 30000);
 
 /* FUNCTIONS */
 function randomNum(max, min, nloop) {
     const randomNumArray = []; //creo array dove vado ad inserire i num
     //faccio un ciclo di n volte
-    let i = 0;
     while (randomNumArray.length < nloop) {
         const randomNumb = Math.floor(Math.random() * (max - min) + min)
 
@@ -29,13 +41,18 @@ function randomNum(max, min, nloop) {
         if (!randomNumArray.includes(randomNumb)) {
             randomNumArray.push(randomNumb);
         }
-
-        i++;
     }
     return randomNumArray
 }
 
+function printArray(DOMel, array) {
+    array.forEach(element => {
+        DOMel.innerText += ` ${element} `;
+    });
+}
+
 function input(nloop) {
+
     const userNum = [];
 
     for (let i = 0; i < nloop; i++) {
