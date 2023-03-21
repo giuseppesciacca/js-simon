a2660bea96aa01b433e2c83d94846e729c58920b
giuseppesciacca <sciaccagi@gmail.com>
2023-03-21 18:03:38 +0100

    [clean] code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 /* Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-
 Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
 Invece di usare prompt e allerte usate inputs ed elementi della dom per mostrare a schermo il risultato. */
 
 const botNumsEl = document.getElementById('bot_nums');
 const resultEl = document.getElementById('result');
+
 //il pc crea 5 numeri casuali tutti diversi e li mette in array
 const botNums = randomNum(100, 0, 5);
 console.log('Array del bot: ' + botNums);
+//li stampo a schermo
+printArray(botNumsEl, botNums);
 //dopo 30 secondi spariscono
 setTimeout(() => {
     botNumsEl.classList.add('d_none');
 }, 30000);
-//li stampo a schermo
-printArray(botNumsEl, botNums);
 
 // prompt 5 volte dopo 30 secondi e mette i numeri nell'array
 setTimeout(() => {
