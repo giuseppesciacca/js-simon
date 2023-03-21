0b4bdb9683c99f8dae270402ba2af801052222f1
giuseppesciacca <sciaccagi@gmail.com>
2023-03-21 16:13:31 +0100

    [clean] code

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,19 @@ Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 Invece di usare prompt e allerte usate inputs ed elementi della dom per mostrare a schermo il risultato. */
 
-const userNum = [];
 const botNums = randomNum(100, 0, 5);
-console.log(botNums);
+console.log('Array del bot: ' + botNums);
 
 // prompt 5 volte e mette i numeri nell'array
-for (let i = 0; i < 5; i++) {
-    const inputNum = Number(prompt('Inserisci numeri:'));
-    userNum.push(inputNum);
-}
-console.log(userNum);
+const userNums = input(5);
+console.log('Array dell\'user: ' + userNums);
 
 // controlla se i numeri nell'userNum sono contenuti nel botNums e quante volte
-let n = 0;
-userNum.forEach(element => {
-    if (botNums.includes(element)) {
-        n++;
-    }
-});
-console.log(`Hai indovinato ${n} numeri`);
+const numChecked = checkNum(userNums, botNums);
+console.log(`Hai indovinato ${numChecked} numeri`);
 
+
+/* FUNCTIONS */
 function randomNum(max, min, nloop) {
     const randomNumArray = []; //creo array dove vado ad inserire i num
     //faccio un ciclo di n volte
@@ -40,4 +33,24 @@ function randomNum(max, min, nloop) {
         i++;
     }
     return randomNumArray
+}
+
+function input(nloop) {
+    const userNum = [];
+
+    for (let i = 0; i < nloop; i++) {
+        const inputNum = Number(prompt('Inserisci numeri:'));
+        userNum.push(inputNum);
+    }
+    return userNum
+}
+
+function checkNum(arrayUser, arrayBot) {
+    let n = 0;
+    arrayUser.forEach(element => {
+        if (arrayBot.includes(element)) {
+            n++;
+        }
+    });
+    return n
 }
