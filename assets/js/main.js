b35df65805f232c9f59c7985d7d1097e6b6c10b4
giuseppesciacca <sciaccagi@gmail.com>
2023-03-21 13:35:02 +0100

    [add] function randomnum

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,21 @@
 Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 Invece di usare prompt e allerte usate inputs ed elementi della dom per mostrare a schermo il risultato. */
+
+
+function randomNum(max, min, nloop) {
+    const randomNumArray = [];
+    let i = 0;
+    while (randomNumArray.length < nloop) {
+        const randomNumb = Math.floor(Math.random() * (max - min) + min)
+
+        if (!randomNumArray.includes(randomNumb)) {
+            randomNumArray.push(randomNumb);
+        }
+
+        i++;
+    }
+    return randomNumArray
+}
+
+console.log(randomNum(100, 0, 5));
